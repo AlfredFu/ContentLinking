e6a4c1385669dc73cc8f08de4a590add24982349
Fred Fu <fred.fu@lexisnexis.com>
2012-06-05 18:02:24 +0800

    keyword hyperlink

--- com/dao/ArticleDAO.py
+++ com/dao/ArticleDAO.py
@@ -3,8 +3,9 @@ from com.dao import *
 class ArticleDAO(DAO):
 	def __init__(self):
 		DAO.__init__(self)
+	
 
-	def update(self):
+	def updateContent(self):
 		pass
 	
 	def updateTime(self):
--- com/dao/CaseDAO.py
+++ com/dao/CaseDAO.py
@@ -7,7 +7,8 @@ class CaseDAO(DAO):
 		DAO.__init__(self)
 	
 	def getAll(self):
-		self.cursor_stg.execute("SELECT case_id,title,origin_id,provider_id,isEnglish FROM cases WHERE isEnglish='Y' AND display=1  limit 0,1;")
+		"获取所有有效案例"
+		self.cursor_stg.execute("SELECT case_id,title,origin_id,provider_id,isEnglish FROM cases WHERE isEnglish='Y' AND display=1;")
 		for row in self.cursor_stg.fetchall():
 			case=Case()
 			case.id=row[0]
@@ -16,18 +17,13 @@ class CaseDAO(DAO):
 			case.providerId=row[3]
 			case.isEnglish=row[4]
 			yield case	
-	
-	def update(self,article):
-		"update content and in_time"
-		#TODO optimize merge following two sql statement
-		#print "UPDATE case_content SET content='%s'" % article.content
-		self.cursor_stg.execute("UPDATE case_content SET content='%s'" % article.content)
+	def getById(self,id):
+		"根据主键信息获取案例"
+		case=Case()
+		return case
 
-
-	def updateTime(self,article):
-		self.cursor_stg.execute("UPDATE cases SET case.in_time=NOW() WHERE case_id=%s" % article.id)
-
-	def getFullCaseByPrimaryKey(self,originId,providerId,isEnglish):
+	def getByKey(self,originId,providerId,isEnglish):
+		"根据origin_id,provider_id,isEnglish获取案例"
 		self.cursor_stg.execute("SELECT cases.case_id,cases.title,case_content.content FROM cases LEFT JOIN case_content ON cases.case_id=case_content.case_id WHERE cases.origin_id=%s AND cases.provider_id=%s AND cases.isEnglish='%s'" % (originId,providerId,isEnglish))
 		row =self.cursor_stg.fetchone()
 		case=Case()
@@ -39,6 +35,15 @@ class CaseDAO(DAO):
 		case.isEnglish=isEnglish
 		return case	
 
+	def updateContent(self,article):
+		"update content and in_time"
+		#TODO optimize merge following two sql statement
+		#print "UPDATE case_content SET content='%s'" % article.content
+		self.cursor_stg.execute("UPDATE case_content SET content='%s' WHERE case_id=%s" % (article.content,article.id))
+
+
+	def updateTime(self,article):
+		self.cursor_stg.execute("UPDATE cases SET case.in_time=NOW() WHERE case_id=%s" % article.id)
 
 if __name__=='__main__':
 	caseDao=CaseDAO()
