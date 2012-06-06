03eb4c0a62e74305c55e32aa7c65f6f0a2bf6e04
Fred Fu <fred.fu@lexisnexis.com>
2012-06-06 17:53:20 +0800

    Keyword entity

---
+++ com/entity/Keyword.py
@@ -0,0 +1,7 @@
+class Keyword(object):
+
+    def __init__(self,content='',status='',type='',fullTitleKeywordId=''):
+        self.content=content
+        self.status=status
+        self.type=type
+        self.fullTitleKeywordId=fullTitleKeywordId
