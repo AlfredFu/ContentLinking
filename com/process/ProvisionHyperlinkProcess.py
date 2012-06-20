04b400bbd07cf364c86d7782a8779ec183432fdd
Fred Fu <fred.fu@lexisnexis.com>
2012-06-20 11:59:14 +0800

    provision hyperlink process

--- com/process/ProvisionHyperlinkProcess.py
+++ com/process/ProvisionHyperlinkProcess.py
@@ -11,6 +11,33 @@ class ProvisionHyperlinkProcess(HyperlinkProcess):
 		#self.provisionPatternStr=r"(?P<astr>article\s+(?P<articleNum>\d+\.?\d+))\s+of\s+the\s+(<a href=\"(?P<href>[^\"^>]*?)\" class=\"link_2\" re=\"T\" cate=\"en_href\"\s*>).+?</a>"
 		self.provisionPatternStr=r'(?P<astr>article\s+(?P<articleNum>[\d+\.]+?))\s+of\s+the [\s"]*?(<a href="(?P<href>[^\"^>]*?)" class="link_2" re="T" cate="en_href"\s*>).+?</a>'
 		self.provisionPattern=re.compile(self.provisionPatternStr,re.I)
+
+	def addProvisionPosTag(self,content):
+		"""
+		给法条添加上位置标记
+		开始标记<a name="i2" re="T"></a>
+		结束标记<a name="end_a148318_att-1_i1" re="T"></a>
+		"""	
+		provisionStartPattern=re.compile(r'(article ([\d\.]+)(.+\n?.+)+)(\n{2,})',re.I)
+		content=provisionStartPattern.sub(r'<a name="i\2" re="T"></a>\1<a name="end_i\2" re="T"></a>\4',content)	
+		#provisionStartPattern=re.compile(r'(\n{2,})(article ([\d\.]+)(.+\n?.+)+)(\n{2,})',re.I)
+		#content=provisionStartPattern.sub(r'\1<a name="i\3" re="T"></a>\2<a name="end_i\3" re="T"></a>\5',content)	
+		return content
+
+	def removeProvisionPosTag(self,content):
+		"""
+		移除法条位置标记
+		"""
+		return re.sub(r'<a name="(end_)?i[\d\.]+" re="T"></a>','',content)
+
+	def checkProvisionExist(self,content,itemId):
+		"""
+		判断法条在文章中是否存在(只在已加法条位置标记后有效)
+		存在返回True,否则返回false(如果在找到多个也返回false,避免hyperlink错误)
+		"""
+		if content.count('<a name="i%s" re="T"></a>' % itemId)==1:
+			return True
+		return False
 	
 	def search(self,content,start=0,posTupleList=[]):
 		tmpContent=content[start:]
@@ -21,6 +48,7 @@ class ProvisionHyperlinkProcess(HyperlinkProcess):
 				endPos=start+matches.end(1)
 				href= matches.group('href')
 				articleNum = matches.group('articleNum')
+				#TODO check provision exist in target article
 				hreflinkTag="<a href=\"%s\" class=\"link_2\" re=\"T\" cate=\"en_href\" >" % (href+"#i"+articleNum,) 
 				posTuple=(startPos,endPos,matches.group('astr'),hreflinkTag)	
 				posTupleList.append(posTuple)
@@ -37,9 +65,16 @@ class ProvisionHyperlinkProcess(HyperlinkProcess):
 			#TODO add cross ref link
 		return article
 
+	def process(self,article):
+		"""
+		重写父类中的process方法
+		"""	
+		if article.contentType == Article.CONTENT_TYPE_LAW:
+			article.content=self.addProvisionPosTag(content)
+		posTupleList=self.search(article.content)
+		for posTupleLis in posTupleList:
+			article=self.pattern(self,article,posTupleList)	
+		return article
+
 if __name__=="__main__":
 	phprocess=ProvisionHyperlinkProcess()
-	content="""
-	The indictment says that Zhang hired another person to forge a resident identity card, and she was found to use this card to apply for overdraft in the bank. Zhang"s act had violated Paragraph 3, Article 280 of the <a href="/law/content.php?content_type=T&origin_id=470853&provider_id=1&isEnglish=Y" class="link_2" re="T" cate="en_href" >Criminal Law of the People"s Republic of China</a> (hereinafter referred to as the <a href="/law/content.php?content_type=T&origin_id=470853&provider_id=1&isEnglish=Y" class="link_2" re="T" cate="en_href" >Criminal Law</a>), and constituted the crime of forging a resident identity card. Jing"an Procuratorate requested Jing"an Court to render a judgment in accordance with the law.
-	"""	
-	print phprocess.search(content)
--- test/testProvisionHyperlinkProcess.py
+++ test/testProvisionHyperlinkProcess.py
@@ -1,6 +1,35 @@
 from com.process.ProvisionHyperlinkProcess import *
 
-if __name__=='__main__':
+def testAddProTag():
+	phprocess=ProvisionHyperlinkProcess()
+	content="""
+Chapter I General Provisions
+Article 1 This Law is enacted for the purposes of regulating the organization and operation of companies, protecting the legitimate rights and interests of companies, shareholders and creditors, maintaining the socialist economic order, and promoting the development of the socialist market economy
+
+Article 2 The term company as referred to in this Law means a limited liability company or a joint stock company limited established within the territory of the Peoples Republic of China in accordance with the provisions of this law
+
+Article 3 A company is an enterprise legal person, which has independent legal person property and enjoys the right to legal person property. It shall bear liability for its debts with all its assets. For a limited liability company, a shareholder shall be liable for the company to the extent of the capital contributions it has paid. For a joint stock limited company, a shareholder shall be liable for the company to the extent of the shares it has subscribed to.
+
+Article 4 The shareholders of a company shall be entitled to enjoy a return on capital, participate in making important decisions, appoint managers and enjoy other shareholder rights.
+
+Article 5 In conducting its business operations, a company shall comply with laws and administrative regulations, social standards, and business standards. It shall act in good faith, accept the supervision of the government and general public, and act in a socially responsible way.
+The legitimate rights and interests of a company shall be protected by law and may not be trespassed against.
+
+Article 6 To establish a company, an application for registration shall be filed with the company registration authority. Where the application meets the establishment requirements of this Law, the company registration authority shall register the company as a limited liability company or joint stock limited company. Where the application does not meet the establishment requirements of this Law, it shall not be registered as a limited liability company or joint stock limited company.
+Article 7 Where any law or administrative regulation provides that the establishment of a company shall be subject to approval, the relevant approval formalities shall be complied with prior to the registration of the company.
+The general public may consult the company register at a company registration authority, which shall allow the general public to consult the register.
+
+"""
+	return phprocess.addProvisionPosTag(content)
+
+def testCheckProvisionExist(content,itemId):
+	phprocess=ProvisionHyperlinkProcess()
+	return phprocess.checkProvisionExist(content,itemId)	
+	
+	
+def testHyperlinkProvi():
+	print phprocess.addProvisionPosTag(content)
+
 	keywordProcess=ProvisionHyperlinkProcess()
 	#keywordProcess.process()
 	i=1
@@ -22,3 +51,7 @@ if __name__=='__main__':
 		print article.content
 		#print article.content
 		keywordProcess.updateArticle(article)
+
+if __name__=='__main__':
+	#print testAddProTag()
+	print testCheckProvisionExist(testAddProTag(),5)
