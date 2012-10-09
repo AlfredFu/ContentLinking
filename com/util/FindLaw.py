741f842b148f23e5a89f5df1da42861c66fed09a
Fred Fu <fred.fu@lexisnexis.com>
2012-10-09 14:42:36 +0800

    resolve duplicate provision mark in legislation's content

--- com/util/FindLaw.py
+++ com/util/FindLaw.py
@@ -7,15 +7,17 @@ from com.process import *
 class FindLaw(HyperlinkProcess):
         def __init__(self):
                 super(FindLaw,self).__init__()  
-                self.illegalProvisionPosTagPattern=re.compile(r'<a re="T" name="(end_)?i[\d\.]+"></a>')
 
         def findIllegal(self):
                 for row in self.lawDao.getAll():
                         law=self.lawDao.getByOrigin(row.originId,row.providerId,row.isEnglish)  
                         if law and law.content:
-                                 if self.illegalProvisionPosTagPattern.search(law.content):
+                                 if self.oldProvisionPosTagPattern.search(law.content):
                                         print law.originId,law.providerId,law.isEnglish 
-
+					self.removeProvisionPosTag(law)
+					self.addProvisionPosTag(law)
+					self.updateArticle(law)
+					self.queueDao.updateStatus(law.id,Article.CONTENT_TYPE_LAW,Article.STATUS_WAIT_UPLOAD)
 if __name__=="__main__":
         lg=FindLaw()
         lg.findIllegal()
